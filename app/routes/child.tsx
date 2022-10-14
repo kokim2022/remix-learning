f2ae7ce08a883e0f8a379edcfdd0dc1c1326ad39
kokim <kokim.tech2020@gmail.com>
2022-10-14 13:13:14 +0630

    route tested

---
+++ app/routes/child.tsx
@@ -0,0 +1,9 @@
+const child = () => {
+    return ( 
+        <div>
+            i am fucking child
+        </div>
+     );
+}
+ 
+export default child;
---
+++ app/routes/child2.tsx
@@ -0,0 +1,9 @@
+const child2 = () => {
+    return ( 
+        <div>
+            I am child 2
+        </div>
+     );
+}
+ 
+export default child2;
