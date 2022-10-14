fa19534902d8af185fc49a4190105e40e01238be
kokim <kokim.tech2020@gmail.com>
2022-10-14 19:43:20 +0630

    film banner ok

--- app/routes/films/$filmId.tsx
+++ app/routes/films/$filmId.tsx
@@ -3,16 +3,22 @@ import { getFileById } from "../../api/films";
 import invariant from "tiny-invariant";
 import { useLoaderData } from "@remix-run/react";
 import type { Film } from "~/api/films";
+import FilmBanner from "~/components/FilmBanner";
 
 export const loader: LoaderFunction = async ({ params }) => {
   invariant(params.filmId, "expected params.filmId"); // error ocuurs only when left side is false.
   const film = await getFileById(params.filmId);
+  console.log("fetching film", film.title);
   return film;
 };
 
 const film = () => {
   const film = useLoaderData<Film>();
-  return <div>{film.title}</div>;
+  return (
+    <div>
+      <FilmBanner film={film} />
+    </div>
+  );
 };
 
-export default film; 
+export default film;
