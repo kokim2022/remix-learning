8e6f29332d29f3b591b91adb13a543f5add20622
kokim <kokim.tech2020@gmail.com>
2022-10-15 10:46:53 +0630

    catch boundary started

--- app/routes/films/$filmId/characters.$characterId.tsx
+++ app/routes/films/$filmId/characters.$characterId.tsx
@@ -1,10 +1,10 @@
-import { LoaderFunction } from "@remix-run/node";
+import type { LoaderFunction } from "@remix-run/node";
 import invariant from "tiny-invariant";
-import { FilmCharacter, getFilmCharacter } from "../../../api/films";
-import { useLoaderData } from "@remix-run/react";
+import type { FilmCharacter } from "../../../api/films";
+import { getFilmCharacter } from "../../../api/films";
+import { useCatch, useLoaderData } from "@remix-run/react";
 
 export const loader: LoaderFunction = async ({ params }) => {
-  console.log("character deail params", params);
   invariant(params.characterId, "expected params.characterId");
   return getFilmCharacter(params.characterId);
 };
@@ -29,3 +29,48 @@ const character = () => {
 };
 
 export default character;
+
+/**
+ * errors expect to happen
+ * @returns
+ */
+export function CatchBoundary() {
+  const caught = useCatch();
+
+  if (caught.status === 404) {
+    return (
+      <div className="mb-3">
+        <div className="text-3xl mb-2">Details</div>
+        <div className="p-4 rounded shadow-lg border bg-orange-200 border-orange-600">
+          <div className="text-gray-700 font-bold text-xl mb-2">
+            {caught.statusText}
+          </div>
+          <p>
+            {caught.status} {caught.statusText}
+          </p>
+        </div>
+      </div>
+    );
+  }
+
+  throw new Error("Unkown error");
+}
+
+/**
+ * errors don't expect to happen
+ * @param param0
+ * @returns
+ */
+export function ErrorBoundary({ error }: any) {
+  return (
+    <div className="mb-3">
+      <div className="text-3xl mb-2">Details</div>
+      <div className="p-4 rounded shadow-lg border bg-rose-200 border-rose-600">
+        <div className="text-gray-700 font-bold text-xl mb-2">
+          Uh oh... Sorry something went wrong!
+        </div>
+        <p>{error?.message}</p>
+      </div>
+    </div>
+  );
+}
